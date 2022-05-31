87378863fb0428f09f96f22a9a6c0341ee3f0829
bekkahhuss <bekkahhuss@gmail.com>
2022-05-30 20:31:35 -0400

    updates to config and db

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,14 @@ const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
+      username: "root",
+      password: "MyPassword",
+      database: "dianas_market",
       dialect: 'mysql',
       dialectOptions: {
         decimalNumbers: true,
       },
+      port: 3306,
     });
 
 module.exports = sequelize;
